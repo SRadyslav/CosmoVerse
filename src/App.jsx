c24c64ffc75eae970e7f01cc8b6f974a47de7495
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-12-11 19:24:51 +0200

    Added background and scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import 'aframe'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
+import sky from './stars.jpg'
+import mountain from './models/mountains.glb?url'
 
 function App() {
-
+  const loader = new GLTFLoader()
+  loader.load( mountain, (d)=>{
+    const entity = document.getElementById("mountain")
+    entity.object3D.add(d.scene)
+  })
   return (
-    <div className="App">
-      App
-    </div>
+    <a-scene>
+      <a-assets>
+        <img id="sky" src={sky} />
+      </a-assets>
+      
+      <a-sky
+      color = "#FFFFFF"
+      material="src: #sky"
+      rotation="0 0 0"
+      >
+      </a-sky>
+      <a-entity id="mountain" position="0 0 0"></a-entity>
+    </a-scene>
   )
 }
 
