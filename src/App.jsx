5eee864c35a06281433ec2545ecadd8df38bf879
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-12-11 18:47:25 +0200

    initalized app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'aframe'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      App
+    </div>
+  )
+}
+
+export default App
